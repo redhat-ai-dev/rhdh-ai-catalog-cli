089b7b2bb32334eedde6630369692a3b4669f7ba
gabemontero <gmontero@redhat.com>
2024-11-13 19:40:31 -0500

    backstage-catalog-rest-cmds

---
+++ pkg/cmd/cli/backstage/constants.go
@@ -0,0 +1,12 @@
+package backstage
+
+const (
+	COMPONENT_TYPE     = "model-server"
+	RESOURCE_TYPE      = "ai-model"
+	API_TYPE           = "openapi"
+	LINK_API_URL       = "API URL"
+	LINK_TYPE_WEBSITE  = "website"
+	LINK_ICON_WEBASSET = "WebAsset"
+	TECHDOC_REFS       = "backstage.io/techdocs-ref"
+	VERSION            = "backstage.io/v1alpha1"
+)
